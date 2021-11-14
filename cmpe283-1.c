e5375543347993aa5939fe89cb91b3ff291c5227
archanashokeeniitg <archanashokeeniitg@gmail.com>
2021-11-14 14:29:17 -0800

    complete file done

--- cmpe283-1.c
+++ cmpe283-1.c
@@ -1,23 +1,38 @@
 /*  
  *  cmpe283-1.c - Kernel module for CMPE283 assignment 1
  */
-#include <linux/module.h>	
-#include <linux/kernel.h>	
+#include <linux/module.h>	/* Needed by all modules */
+#include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include <asm/msr.h>
 
 #define MAX_MSG 80
 
+/*
+ * Model specific registers (MSRs) by the module.
+ * See SDM volume 4, section 2.1
+ */
 #define IA32_VMX_PINBASED_CTLS	0x481
 #define IA32_VMX_PROCBASED_CTLS	0x482
 #define IA32_VMX_PROCBASED_CTLS2	0x48B
 #define IA32_VMX_EXIT_CTLS	0x483
 #define IA32_VMX_ENTRY_CTLS	0x484
 
+/*
+ * struct caapability_info
+ *
+ * Represents a single capability (bit number and description).
+ * Used by report_capability to output VMX capabilities.
+ */
 struct capability_info {
 	uint8_t bit;
 	const char *name;
 };
 
+
+/*
+ * Pinbased capabilities
+ * See SDM volume 3, section 24.6.1
+ */
 struct capability_info pinbased[5] =
 {
 	{ 0, "External Interrupt Exiting" },
@@ -27,6 +42,10 @@ struct capability_info pinbased[5] =
 	{ 7, "Process Posted Interrupts" }
 };
 
+/*
+ * VM Exit capabilities
+ * See SDM volume 3, section 24.7.1
+ */
 struct capability_info vmExit[14] =
 {
 	{ 2, "Save debug controls" },
@@ -45,6 +64,10 @@ struct capability_info vmExit[14] =
 	{ 29, "Load PKRS" }
 };
 
+/*
+ * VM Entry capabilities
+ * See SDM volume 3, section 24.8.1
+ */
 struct capability_info vmEntry[12] =
 {
 	{ 2, "Load debug controls" },
@@ -61,6 +84,11 @@ struct capability_info vmEntry[12] =
 	{ 22, "Load PKRS" }
 };
 
+
+/*
+ * Procbased capabilities
+ * See SDM volume 3, section 24.6.2
+ */
 struct capability_info procbased1[21] =
 {
 	{ 2, "Interrupt-window exiting" },
@@ -116,6 +144,19 @@ struct capability_info procbased2[23] =
 	{ 22, "Mode-based execute control for EPT" },
 	{ 25, "Use TSC scaling"}
 };
+
+/*
+ * report_capability
+ *
+ * Reports capabilities present in 'cap' using the corresponding MSR values
+ * provided in 'lo' and 'hi'.
+ *
+ * Parameters:
+ *  cap: capability_info structure for this feature
+ *  len: number of entries in 'cap'
+ *  lo: low 32 bits of capability MSR value describing this feature
+ *  hi: high 32 bits of capability MSR value describing this feature
+ */
 void
 report_capability(struct capability_info *cap, uint8_t len, uint32_t lo,
     uint32_t hi)
@@ -136,6 +177,11 @@ report_capability(struct capability_info *cap, uint8_t len, uint32_t lo,
 	}
 }
 
+/*
+ * detect_vmx_features
+ *
+ * Detects and prints VMX capabilities of this host's CPU.
+ */
 void
 detect_vmx_features(void)
 {
@@ -178,6 +224,14 @@ detect_vmx_features(void)
 	}
 }
 
+/*
+ * init_module
+ *
+ * Module entry point
+ *
+ * Return Values:
+ *  Always 0
+ */
 int
 init_module(void)
 {
@@ -185,10 +239,17 @@ init_module(void)
 
 	detect_vmx_features();
 
-
+	/* 
+	 * A non 0 return means init_module failed; module can't be loaded. 
+	 */
 	return 0;
 }
 
+/*
+ * cleanup_module
+ *
+ * Function called on module unload
+ */
 void
 cleanup_module(void)
 {
